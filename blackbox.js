8cc3ce91a76eaf2497400f8a0eba88e31a825d67
Usr <j46xue@edu.uwaterloo.ca>
2020-01-28 14:21:09 -0500

    blank

--- blackbox.js
+++ blackbox.js
@@ -104,7 +104,7 @@ function formToResults() {
     return false;
 }
 
-document.getElementById("submitButton").addEventListener('click', formToResults().bind(this));
+document.getElementById("submitButton").addEventListener('click', formToResults().call());
 
 
 /*
