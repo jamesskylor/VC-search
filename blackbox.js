895e71016b16390ed84ff3e0ddac1b0e4de03496
Usr <j46xue@edu.uwaterloo.ca>
2020-01-30 14:23:34 -0500

    Deleted redundant comments

--- blackbox.js
+++ blackbox.js
@@ -1,8 +1,7 @@
 
 /*
 To Do:
-- Code the way you recieve the JSON file
-- Code the way to recieve the answers to the form
+- Test and Fix the results printing
 */
 
 // Create an event listener for the submission of the form
@@ -157,42 +156,6 @@ function formToResults() {
 }
 
 /*
-Ascending loop of selections
-    Check if <= 0 vc then continue
-    Create h1 for that table
-    Create table
-    Loop through VCs
-        check if current loopVal % 2 == 0
-            Create row and append to table
-        Create cell and append in current row
-        Append h2 in current cell
-        Create p w/ information
-        Append p in current cell
-    Create space to next table
-*/
-
-/*
-
-// Get the JSON file first and other pre stuff
-// Get info on what the searcher is looking for
-
-var jsonFile = JSON.parse();
-var srchStage = ""; // Mandatory
-var srchLoc = ""; // Matching = +1 pt
-var srchSector = []; // Matching = +2 pts per match
-
-// Set up finder thing
-var maxPts = (srchSector.length * 2) + 1;
-var selections = [];
-var repeat;
-for (repeat = 0; repeat < maxPts; repeat += 1) {
-    selections.push([]); // Push in a new empty array
-}
-// Create an array that holds key of maxPoint : Set<All companies with matching # points>
-
-*/
-
-/*
 
 Format for JSON file
 
@@ -221,44 +184,6 @@ Format for JSON file
 ]
 
 */
-
-/*
-
-// Add up points -1 and if > 0 (since only 1pt (-1pt = 0pt) if only location match which is no good), put into selections[pts].push(jsonFile[ind])
-var tempPts;
-var ind;
-for (ind = 0; ind < jsonFile.length; ind += 1) {
-    
-    // Set to -1 to reset to default value
-    tempPts = -1;
-    
-    // Only care at all if the stage matches
-    if (new Set(jsonFile[ind].stage).has(srchStage)) { // Fix w/ set.has() srchStage
-        
-        // Search if same location and assign a point if so
-        // Only check while tempPts == 0 since if it == 1, then it's already found it's answer
-        var locSrch;
-        if(new Set(jsonFile[ind].location).has(srchLoc)) {
-            tempPts += 1;
-        }
-        
-        // Now search for matches in sectors
-        // Create 2 sets, set estSize to their sizes added together, union them and tempPts+=2*(estSize-union.size);
-        var estSize = srchSector.length + jsonFile[ind].sectors.length;
-        var set = new Set(srchSector);
-        jsonFile[ind].sectors.forEach(set.add, set);
-        tempPts += 2 * (estSize - set.size);
-        
-        // Put the investor into an array storing the information on how good a match they are
-        if (tempPts > 0) {
-            selections[tempPts].push(jsonFile[ind]);
-        }
-    }
-}
-
-*/
-
-
 
 
 
