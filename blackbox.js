a1fc763ea15de81a6328f5cbbc47506f26c72e98
Jackie <jackieliu191@gmail.com>
2020-01-30 16:09:33 -0500

    commit

--- blackbox.js
+++ blackbox.js
@@ -102,7 +102,7 @@ function formToResults() {
         } 
     }
     console.log(srchSector);
-        console.log(srchLoc)
+        console.log(srchLoc);
     /*
     // Replace the URL with that of the results screen
      location.replace("https://jackiehj-liu.github.io/atom-capital/results.html");
