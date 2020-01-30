b08862b5ae410e4e7a52356f3171e6998247d417
Jackie <jackieliu191@gmail.com>
2020-01-30 14:59:40 -0500

    commit

---
+++ csv-to-json.js
@@ -0,0 +1,7 @@
+ Papa.parse("https://jackiehj-liu.github.io/atom-capital/atom-capital.csv", {
+	download: true,
+    header: true,
+	complete: function(results) {
+		console.log(results);
+	}
+     });
